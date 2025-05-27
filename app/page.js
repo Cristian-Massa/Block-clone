30ccc16b1eada011464e88c9fcef5347c666572a
Cristian-Massa <cristianmassa30@gmail.com>
2025-05-27 12:50:42 -0300

    remove page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
-
 import { Canvas } from "@/components/ui/Scene/Canvas";
-import styles from "../page.module.css";
+import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.layout}>
